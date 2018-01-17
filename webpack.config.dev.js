9f373a6737b51878f2685ffe92090cca417f99b2
Frank Garcia <futboler18@yahoo.com>
2018-01-17 11:47:04 -0800

    Hello world base project

---
+++ webpack.config.dev.js
@@ -0,0 +1,30 @@
+var path = require('path');
+
+var webpackMerge = require('webpack-merge');
+var commonConfig = require('./webpack.config.common.js');
+
+module.exports = webpackMerge(commonConfig, {
+    devtool: 'cheap-module-eval-source-map',
+
+    output: {
+        path: path.resolve(__dirname + '/public/js/app'),
+        publicPath: "/js/app/",
+        filename: 'bundle.js',
+        chunkFilename: '[id].chunk.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    {loader: 'awesome-typescript-loader', options: {
+                        transpileOnly: true
+                    }},
+                    {loader: 'angular2-template-loader'},
+                    {loader: 'angular-router-loader'}
+                ]
+            }
+        ]
+
+    }
+});
