1131e4c74957dde4fb8b177d6d7b0b14ed5b1f0a
Frank Garcia <futboler18@yahoo.com>
2018-01-17 20:34:58 -0800

    Added ExpressJS Routes for updating a page with an input

--- routes/app.js
+++ routes/app.js
@@ -5,4 +5,13 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
+router.get('/message/:msg', function (req, res, next) {
+    res.render('node', {message: req.params.msg});
+});
+
+router.post('/message', function(req, res, next) {
+    var message = req.body.message;
+    res.redirect('/message/' + message);
+});
+
 module.exports = router;
