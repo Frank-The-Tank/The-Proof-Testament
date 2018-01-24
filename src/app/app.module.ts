ee77898d98d4b884be32b5a54208d38eed41de77
giorgio19 <giorgio.catania@pepperdine.edu>
2018-01-24 00:11:44 -0800

    fixed the text editor. Now using open source CKEditor which has a lot of built in functionality and formatting which we can use.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CKEditorModule } from 'ng2-ckeditor';
 
-
-import { AppComponent } from './app.component';
+import { AppComponent, SampleEditor } from './app.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    SampleEditor
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CKEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
