472696e8fc5bc55636427dcbbc65379f1c427e39
giorgio19 <giorgio.catania@pepperdine.edu>
2018-01-23 17:08:21 -0800

    fixing editor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { EditingComponent } from './editing/editing.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditingComponent
   ],
   imports: [
     BrowserModule
--- src/app/editor/editor.component.spec.ts
+++ src/app/editor/editor.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EditingComponent } from './editing.component';
+import { EditorComponent } from './editor.component';
 
-describe('EditingComponent', () => {
-  let component: EditingComponent;
-  let fixture: ComponentFixture<EditingComponent>;
+describe('EditorComponent', () => {
+  let component: EditorComponent;
+  let fixture: ComponentFixture<EditorComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EditingComponent ]
+      declarations: [ EditorComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(EditingComponent);
+    fixture = TestBed.createComponent(EditorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
