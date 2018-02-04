ebaf7cfa33e4d51252f6deb7c2d914fe4f297644
jamyers2 <myers.a.joshua@gmail.com>
2018-02-03 18:19:51 -0800

    Got rid of bad HTML 'layout <<<<', import editor class

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Editor } from '../app.component';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
+
 export class FooterComponent implements OnInit {
 
+  @Input() editor: Editor;
+
+  // test: string = "test"
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addProof() {
+    var node = document.getElementsByClassName("col-md-7");
+    var newEditor = document.createTextNode("water");
+    node.appendChild(newEditor);
+  
   }
 
 }
