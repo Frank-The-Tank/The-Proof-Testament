9f6595fe3206517d13cd0820738b9244a5844b53
jamyers2 <myers.a.joshua@gmail.com>
2018-02-05 17:01:02 -0800

    bug fixes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Editor } from '../app.component'
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
+
 export class FooterComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addProof() {
+    alert('hi');
   }
 
 }
