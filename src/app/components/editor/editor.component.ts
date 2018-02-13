26bcc8133784a6880effcc0bf3adcb572941eda5
giorgio19 <giorgio.catania@pepperdine.edu>
2018-02-12 22:02:17 -0800

    okay so delta is now working, if you type something in editor and inspect the page in chrome then look at the console you'll see it shows what you are typing and if you click away/to a different line or use arrow key to go down

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 
@@ -9,10 +9,6 @@ import 'rxjs/add/operator/distinctUntilChanged';
 
 import Quill from 'quill';
 
-// // // add image resize module
-// // import ImageResize from 'quill-image-resize-module';
-// // Quill.register('modules/imageResize', ImageResize);
-//
 // // override p with div tag
 // const Parchment = Quill.import('parchment');
 // let Block = Parchment.query('block');
@@ -23,12 +19,7 @@ import Quill from 'quill';
 // //
 // // import Counter from './counter';
 // // Quill.register('modules/counter', Counter)
-//
-// // Add fonts to whitelist
-// var Font = Quill.import('formats/font');
-// // We do not add Aref Ruqaa since it is the default
-// Font.whitelist = ['mirza', 'aref', 'sans-serif', 'monospace', 'serif'];
-// Quill.register(Font, true);
+
 
 @Component({
   selector: 'app-editor',
@@ -36,7 +27,9 @@ import Quill from 'quill';
   styleUrls: ['./editor.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class EditorComponent{
+
+
+export class EditorComponent {
   // title = content already in the editor
   title = '<p> Prove: </p> ' +
     '<p> Description: By ... </p> ' +
@@ -53,10 +46,53 @@ export class EditorComponent{
   placeholder = 'placeholder';
   form: FormGroup;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      editor: ['test']
+    });
+  }
+  @ViewChild('editor') editor: QuillEditorComponent
 
   ngOnInit() {
+    this.form
+      .controls
+      .editor
+      .valueChanges
+      .debounceTime(400)
+      .distinctUntilChanged()
+      .subscribe(data => {
+        console.log('native fromControl value changes with debounce', data)
+      });
 
+    this.editor
+      .onContentChanged.debounceTime(400)
+      .distinctUntilChanged()
+      .subscribe(data => {
+        console.log('view child + directly subscription', data)
+      });
   }
 
+  setControl() {
+    this.form.setControl('editor', new FormControl('test - new Control'))
+  }
+
+  setFocus($event) {
+    $event.focus();
+  }
+
+  patchValue() {
+    this.form.controls['editor'].patchValue(`${this.form.controls['editor'].value} patched!`)
+  }
+
+  toggleReadOnly() {
+    this.isReadOnly = !this.isReadOnly;
+  }
+
+  logChange($event: any) {
+    console.log($event);
+  }
+
+  logSelection($event: any) {
+    console.log($event);
+  }
 }
