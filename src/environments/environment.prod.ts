82861dccadbc965d42bfef446a6a3b941b824858
Frank-The-Tank <futboler101@yahoo.com>
2018-02-12 17:12:48 -0800

    Added a production server and corresponding Firebase Host Webpage to the project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,11 @@
 export const environment = {
   production: true,
-  apiKey: "AIzaSyBH7En_vRHHMw0vZlt6GkGgp0_lOvtUqac",
-  authDomain: "the-proof-testament.firebaseapp.com",
-  databaseURL: "https://the-proof-testament.firebaseio.com",
-  projectId: "the-proof-testament",
-  storageBucket: "the-proof-testament.appspot.com",
-  messagingSenderId: "388835831512"
+  firebase: {
+    apiKey: "AIzaSyBH7En_vRHHMw0vZlt6GkGgp0_lOvtUqac",
+    authDomain: "the-proof-testament.firebaseapp.com",
+    databaseURL: "https://the-proof-testament.firebaseio.com",
+    projectId: "the-proof-testament",
+    storageBucket: "the-proof-testament.appspot.com",
+    messagingSenderId: "388835831512"
+  }
 };
