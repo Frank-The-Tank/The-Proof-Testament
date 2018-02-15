57b866f0045bc7780f52ca08c1d5b3a03f428cbc
giorgio19 <giorgio.catania@pepperdine.edu>
2018-02-14 19:31:21 -0800

    added more to regexp, definitely flawed, but helps simulate final functionality to be able to add inline symbols. Will fix later

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,9 +1,9 @@
 import {Component, ElementRef, OnInit, ViewChild, ViewEncapsulation, ViewContainerRef, ComponentFactoryResolver} from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Observable } from 'rxjs/Observable';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
 
-import { QuillEditorComponent } from 'ngx-quill/src/quill-editor.component';
-import { AutocompleteBoxComponent } from '../autocomplete-box/autocomplete-box.component';
+import {QuillEditorComponent} from 'ngx-quill/src/quill-editor.component';
+import {AutocompleteBoxComponent} from '../autocomplete-box/autocomplete-box.component';
 
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
@@ -25,21 +25,21 @@ export class EditorComponent implements OnInit {
   title = '<p>Prove: </p> ' +
     '<p>Description: By ... </p> ' +
     '<br>Proof: <br> ';
-    // '<p> /≡=¬≢≠≥≤⇒⇐⇍⇏≔<>∈∅Ʊ⊂⊃⊆⊇∉⊄⊅⊈⊉∪∩#~⋅*∘∙÷×Ρ↓↑◃▹★∀∃⋁⋀+-^ </p>';
-    isReadOnly = false;
-    placeholder = 'placeholder';
-    form: FormGroup;
-    modules = {};
+  // '<p> /≡=¬≢≠≥≤⇒⇐⇍⇏≔<>∈∅Ʊ⊂⊃⊆⊇∉⊄⊅⊈⊉∪∩#~⋅*∘∙÷×Ρ↓↑◃▹★∀∃⋁⋀+-^ </p>';
+  isReadOnly = false;
+  placeholder = 'placeholder';
+  form: FormGroup;
+  modules = {};
 
-    constructor(fb: FormBuilder, private factoryResolver: ComponentFactoryResolver) {
-      this.form = fb.group({
-        editor: ['test']
-      });
+  constructor(fb: FormBuilder, private factoryResolver: ComponentFactoryResolver) {
+    this.form = fb.group({
+      editor: ['test']
+    });
 
-      this.modules = {
-        formula: true,
-        toolbar: [[{ 'indent': '-1'}, { 'indent': '+1' }], ['formula']]
-      };
+    this.modules = {
+      formula: true,
+      toolbar: [[{'indent': '-1'}, {'indent': '+1'}], ['formula']]
+    };
   }
 
   @ViewChild('editor') editor: QuillEditorComponent;
@@ -61,22 +61,24 @@ export class EditorComponent implements OnInit {
       .subscribe(data => {
         console.log('view child + directly subscription', data);
       });
-    }
+  }
 
-    keyPressed() {
-      console.log('TEST WORKED');
-      const factory = this.factoryResolver.resolveComponentFactory(AutocompleteBoxComponent);
-      const ref = this.viewContainerRef.createComponent(factory);
-      ref.changeDetectorRef.detectChanges();
-    }
+  keyPressed() {
+    console.log('TEST WORKED');
+    const factory = this.factoryResolver.resolveComponentFactory(AutocompleteBoxComponent);
+    const ref = this.viewContainerRef.createComponent(factory);
+    ref.changeDetectorRef.detectChanges();
+  }
 
-    addBindingCreated(quill) {
+  addBindingCreated(quill) {
 
-      //implies
-      quill.keyboard.addBinding({ key: 'i' }, {
-          collapsed: true,
-          prefix: /^=$/,
-          offset: 1,
+    //rule definition symbol
+
+    //implies
+    quill.keyboard.addBinding({key: 'i'}, {
+        collapsed: true,
+        prefix: /^=$/,
+        offset: 1,
       },
       (range, context) => {
         quill.deleteText(range.index - 1, 1); // range.index-1 = user's cursor -1 -> where = character is
@@ -84,64 +86,76 @@ export class EditorComponent implements OnInit {
         quill.setSelection(range.index + 11);
       });
 
-
-      quill.keyboard.addBinding({ key: 'i' }, {
-          collapsed: true,
-          prefix: /^([a-z]*\s*)*=$/
+    //follows from
+    quill.keyboard.addBinding({key: 'f'}, {
+        collapsed: true,
+        prefix: /^=$/,
+        offset: 1,
       },
       (range, context) => {
         quill.deleteText(range.index - 1, 1); // range.index-1 = user's cursor -1 -> where = character is
-        quill.insertText(range.index - 1, '⇒ ');
+        quill.insertText(range.index - 1, '⇐          <>');
+        quill.setSelection(range.index + 11);
       });
 
-      //follows from
-      quill.keyboard.addBinding({ key: 'f' }, {
-          collapsed: true,
-          prefix: /^=$/,
-          offset: 1,
+    //equals
+    quill.keyboard.addBinding({key: 'e'}, {
+        collapsed: true,
+        prefix: /^=$/,
+        offset: 1,
       },
-       (range, context) => {
-         quill.deleteText(range.index - 1, 1); // range.index-1 = user's cursor -1 -> where = character is
-         quill.insertText(range.index - 1, '⇐          <>');
-         quill.setSelection(range.index + 11);
+      (range, context) => {
+        quill.deleteText(range.index - 1, 1); // range.index-1 = user's cursor -1 -> where = character is
+        quill.insertText(range.index - 1, '=            <>');
+        quill.setSelection(range.index + 13);
       });
 
-      quill.keyboard.addBinding({ key: 'f' }, {
-          collapsed: true,
-          prefix: /^([a-z]*\s*)*=$/
+    //less than
+    quill.keyboard.addBinding({key: 'l'}, {
+        collapsed: true,
+        prefix: /^=$/,
+        offset: 1,
+      },
+      (range, context) => {
+        quill.deleteText(range.index - 1, 1); // range.index-1 = user's cursor -1 -> where = character is
+        quill.insertText(range.index - 1, '<            <>');
+        quill.setSelection(range.index + 13);
+      });
+
+
+    //greater than
+    quill.keyboard.addBinding({key: 'g'}, {
+
+        collapsed: true,
+        prefix: /^=$/,
+        offset: 1,
+      },
+      (range, context) => {
+        quill.deleteText(range.index - 1, 1); // range.index-1 = user's cursor -1 -> where = character is
+        quill.insertText(range.index - 1, '>            <>');
+        quill.setSelection(range.index + 13);
+      });
+
+    // ///////////////////////////////////////////inline symbols///////////////////////////////////////////
+
+    // follows from
+    quill.keyboard.addBinding({key: 'f'}, {
+        empty: false,
+        collapsed: true,
+        prefix: /^([A-Z]|[a-z]|\s|:|≡|=|¬|≢|≠|≥|≤|⇒|⇐|⇍|⇏|≔|<|>|∈|∅|Ʊ|⊂|⊃|⊆|⊇|∉|⊄|⊅|⊈|⊉|∪|∩|#|~|⋅|∘|∙|÷|×|Ρ|↓|↑|◃|▹|★|∀|∃|⋁|⋀)*=$/
       },
       (range, context) => {
         quill.deleteText(range.index - 1, 1); // range.index-1 = user's cursor -1 -> where = character is
         quill.insertText(range.index - 1, '⇐ ');
       });
 
-      //equals
-      quill.keyboard.addBinding({ key: 'e' }, {
-          collapsed: true,
-          prefix: /^=$/,
-          offset: 1,
-      },
-       (range, context) => {
-         quill.deleteText(range.index - 1, 1); // range.index-1 = user's cursor -1 -> where = character is
-         quill.insertText(range.index - 1, '=            <>');
-         quill.setSelection(range.index + 13);
-      });
+    //less than
+    quill.keyboard.addBinding({key: 'l'}, {
+        empty: false,
+        collapsed: true,
+        prefix: /^([A-Z]|[a-z]|\s|:|≡|=|¬|≢|≠|≥|≤|⇒|⇐|⇍|⇏|≔|<|>|∈|∅|Ʊ|⊂|⊃|⊆|⊇|∉|⊄|⊅|⊈|⊉|∪|∩|#|~|⋅|∘|∙|÷|×|Ρ|↓|↑|◃|▹|★|∀|∃|⋁|⋀)*=$/
 
-      //less than
-      quill.keyboard.addBinding({ key: 'l' }, {
-          collapsed: true,
-          prefix: /^=$/,
-          offset: 1,
-      },
-       (range, context) => {
-         quill.deleteText(range.index - 1, 1); // range.index-1 = user's cursor -1 -> where = character is
-         quill.insertText(range.index - 1, '<            <>');
-         quill.setSelection(range.index + 13);
-      });
-
-      quill.keyboard.addBinding({ key: 'l' }, {
-          collapsed: true,
-          prefix: /^([a-z]*\s*)*=$/
+          //missing * and - and + characters and ^
       },
       (range, context) => {
         quill.deleteText(range.index - 1, 1); // range.index-1 = user's cursor -1 -> where = character is
@@ -149,43 +163,66 @@ export class EditorComponent implements OnInit {
       });
 
 
-      //greater than
-      quill.keyboard.addBinding({ key: 'g' }, {
-          collapsed: true,
-          prefix: /^=$/,
-          offset: 1,
-      },
-       (range, context) => {
-         quill.deleteText(range.index - 1, 1); // range.index-1 = user's cursor -1 -> where = character is
-         quill.insertText(range.index - 1, '>            <>');
-         quill.setSelection(range.index + 13);
-      });
-
-      quill.keyboard.addBinding({ key: 'g' }, {
-          collapsed: true,
-          prefix: /^([a-z]*\s*)*=$/
+    //greater than
+    quill.keyboard.addBinding({key: 'g'}, {
+        empty: false,
+        collapsed: true,
+        prefix: /^([A-Z]|[a-z]|\s|:|≡|=|¬|≢|≠|≥|≤|⇒|⇐|⇍|⇏|≔|<|>|∈|∅|Ʊ|⊂|⊃|⊆|⊇|∉|⊄|⊅|⊈|⊉|∪|∩|#|~|⋅|∘|∙|÷|×|Ρ|↓|↑|◃|▹|★|∀|∃|⋁|⋀)*=$/
       },
       (range, context) => {
         quill.deleteText(range.index - 1, 1); // range.index-1 = user's cursor -1 -> where = character is
         quill.insertText(range.index - 1, '> ');
       });
 
-    }
+    //implies
+    quill.keyboard.addBinding({key: 'i'}, {
+        empty: false,
+        collapsed: true,
+        prefix: /^([A-Z]|[a-z]|\s|:|≡|=|¬|≢|≠|≥|≤|⇒|⇐|⇍|⇏|≔|<|>|∈|∅|Ʊ|⊂|⊃|⊆|⊇|∉|⊄|⊅|⊈|⊉|∪|∩|#|~|⋅|∘|∙|÷|×|Ρ|↓|↑|◃|▹|★|∀|∃|⋁|⋀)*=$/
+      },
+      (range, context) => {
+        quill.deleteText(range.index - 1, 1); // range.index-1 = user's cursor -1 -> where = character is
+        quill.insertText(range.index - 1, '⇒ ');
+      });
 
-    setControl() {
-      this.form.setControl('editor', new FormControl('test - new Control'));
-    }
+    //union
+    quill.keyboard.addBinding({key: 'u'}, {
+        empty: false,
+        collapsed: true,
+        prefix: /^([A-Z]|[a-z]|\s|:|≡|=|¬|≢|≠|≥|≤|⇒|⇐|⇍|⇏|≔|<|>|∈|∅|Ʊ|⊂|⊃|⊆|⊇|∉|⊄|⊅|⊈|⊉|∪|∩|#|~|⋅|∘|∙|÷|×|Ρ|↓|↑|◃|▹|★|∀|∃|⋁|⋀)*=$/
+      },
+      (range, context) => {
+        quill.deleteText(range.index - 1, 1); // range.index-1 = user's cursor -1 -> where = character is
+        quill.insertText(range.index - 1, '∪ ');
+      });
 
-    setFocus($event) {
-      $event.focus();
-    }
-
-    logChange($event: any) {
-      console.log($event);
-    }
-
-    logSelection($event: any) {
-      console.log($event);
-    }
+    //intersection = dis-union -> d
+    quill.keyboard.addBinding({key: 'd'}, {
+        empty: false,
+        collapsed: true,
+        prefix: /^([A-Z]|[a-z]|\s|:|≡|=|¬|≢|≠|≥|≤|⇒|⇐|⇍|⇏|≔|<|>|∈|∅|Ʊ|⊂|⊃|⊆|⊇|∉|⊄|⊅|⊈|⊉|∪|∩|#|~|⋅|∘|∙|÷|×|Ρ|↓|↑|◃|▹|★|∀|∃|⋁|⋀)*=$/
+      },
+      (range, context) => {
+        quill.deleteText(range.index - 1, 1); // range.index-1 = user's cursor -1 -> where = character is
+        quill.insertText(range.index - 1, '∩ ');
+      });
 
   }
+
+  setControl() {
+    this.form.setControl('editor', new FormControl('test - new Control'));
+  }
+
+  setFocus($event) {
+    $event.focus();
+  }
+
+  logChange($event: any) {
+    console.log($event);
+  }
+
+  logSelection($event: any) {
+    console.log($event);
+  }
+
+}
