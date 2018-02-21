9947ffe3156df6c5f3460c36ecd53001eec39bad
Micah Benn <hidden@domain.com>
2018-02-20 18:07:06 -0800

    Grabbing text (readying for export functionality).

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewContainerRef, ComponentFactoryResolver, ViewChild } from '@angular/core';
 import { EditorComponent } from '../editor/editor.component';
 
+import * as Quill from 'quill';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -22,7 +24,19 @@ export class HomeComponent implements OnInit {
   }
 
   export() {
-    
-  }
 
+    const textBoxes = document.getElementsByClassName("ql-editor");
+
+    // console.log(container.getElementsByTagName('p')[0].textContent);
+
+    var output = "";
+
+    for (var i = 0; i < textBoxes.length; i++) {
+      var textBox = textBoxes[i];
+
+      output += "# Exercise " + (i + 1) + "\n" + textBox.innerHTML + "\n";
+    }
+
+    console.log(output);
+  }
 }
