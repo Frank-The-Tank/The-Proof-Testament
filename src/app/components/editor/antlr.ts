3f7b2adab16ea2b36c99b116f25bbd5402beb307
giorgio19 <giorgio.catania@pepperdine.edu>
2018-03-31 14:35:52 -0700

    beginning to buiild module to pass quill text into antlr component

--- src/app/components/editor/antlr.ts
+++ src/app/components/editor/antlr.ts
@@ -1,1 +1,16 @@
 //will replace fm2latex, takes text from quill and parses with AntlrComponent
+
+export interface Config {
+  container: string;
+  unit: 'word'|'char';
+}
+
+export interface QuillInstance {
+  on: Function;
+  getText: Function;
+}
+
+export default class AntlrGrammar {
+  quill: QuillInstance;
+  options: Config;
+}
